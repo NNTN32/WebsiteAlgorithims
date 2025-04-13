0a43e92b86f585b079c290bbd64d0651665ec48b
nhannguyen <thanhnhanna102003@gmail.com>
2025-04-13 23:07:41 +0700

    BuildFirstStep

---
+++ src/main/java/com/example/WebsiteAlgorithm/WebsiteAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebsiteAlgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebsiteAlgorithm/WebsiteAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebsiteAlgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
