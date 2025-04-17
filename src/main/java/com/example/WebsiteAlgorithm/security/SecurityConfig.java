f3dcff5b0e2d1122dc60788ded0d51e589670e35
nhannguyen <thanhnhanna102003@gmail.com>
2025-04-17 15:52:07 +0700

    AddSpringSecurity

--- src/main/java/com/example/WebsiteAlgorithm/security/SecurityConfig.java
+++ src/main/java/com/example/WebsiteAlgorithm/security/SecurityConfig.java
@@ -1,4 +1,33 @@
 package com.example.WebsiteAlgorithm.security;
 
+import com.example.WebsiteAlgorithm.config.JwtAuthenticationFilter;
+import com.example.WebsiteAlgorithm.config.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenProvider jwtTokenProvider) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login")
+                        .permitAll()
+
+                        .requestMatchers("http://127.0.0.1:5500", "http://localhost:5500").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
