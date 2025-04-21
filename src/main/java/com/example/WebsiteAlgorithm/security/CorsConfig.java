372ed886dabaf299726a6398ef13fbb65abe74e5
nhannguyen <thanhnhanna102003@gmail.com>
2025-04-21 18:50:07 +0700

    FixCorsAgain

--- src/main/java/com/example/WebsiteAlgorithm/security/CorsConfig.java
+++ src/main/java/com/example/WebsiteAlgorithm/security/CorsConfig.java
@@ -13,10 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174") // FE React
+                        .allowedOriginPatterns("*") // hỗ trợ wildcard
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(true); // vẫn dùng được
             }
         };
     }
