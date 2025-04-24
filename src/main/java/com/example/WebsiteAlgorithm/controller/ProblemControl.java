30c76fdd49189f4f1e47a9c730b24605d70f355f
nhannguyen <thanhnhanna102003@gmail.com>
2025-04-24 12:39:45 +0700

    AuthAPIListProblemID

--- src/main/java/com/example/WebsiteAlgorithm/controller/ProblemControl.java
+++ src/main/java/com/example/WebsiteAlgorithm/controller/ProblemControl.java
@@ -1,5 +1,6 @@
 package com.example.WebsiteAlgorithm.controller;
 
+import com.example.WebsiteAlgorithm.config.JwtTokenProvider;
 import com.example.WebsiteAlgorithm.dto.Auth.Testcase.TestRequest;
 import com.example.WebsiteAlgorithm.model.Problem;
 import com.example.WebsiteAlgorithm.service.ProblemService;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +19,9 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/problem")
 @RequiredArgsConstructor
 public class ProblemControl {
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
     @Autowired
     private ProblemService problemService;
 
@@ -43,13 +48,32 @@ public class ProblemControl {
     }
 
     @GetMapping("/{problemId}")
-    public ResponseEntity<?> getProblemDetails(@PathVariable Long problemId) {
+    public ResponseEntity<?> getProblemDetails(
+            @PathVariable Long problemId,
+            @RequestHeader("Authorization") String authHeader) {
+
         try {
-            // Gọi service để lấy thông tin Problem
+            //Lấy token từ header
+            if (!StringUtils.hasText(authHeader) || !authHeader.startsWith("Bearer ")) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing or invalid Authorization header");
+            }
+            String token = authHeader.substring(7); // Cắt "Bearer "
+
+            // Xác thực token
+            if (!tokenProvider.validateToken(token)) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or expired token");
+            }
+
+            // Check role là USER
+            String role = tokenProvider.extractClaim(token, claims -> claims.get("role", String.class));
+            if (!"USER".equals(role)) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied: Role must be USER");
+            }
+
+            // Gọi service lấy chi tiết bài toán
             ResponseEntity<Map<String, Object>> response = problemService.getProblemDetailsById(problemId);
             return response;
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: " + e.getMessage());
+
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected error: " + e.getMessage());
         }
